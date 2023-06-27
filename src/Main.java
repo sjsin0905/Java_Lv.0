ebf0151c07740b92dfb609800814cb8d03bcf078
조용훈 <sjsin0905@naver.com>
2023-06-27 20:23:05 +0900

    자바 출력 공부

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        String a = "a";
+        float pi = 3.141592F;
+        float ppi = 30000000.001F;
         System.out.println("Hello world!");
+        System.out.printf("I like %s", a);
+        System.out.printf("pi: %10.2f\n",pi);
+        System.out.printf("pi: %+,020.2f",ppi);
     }
 }
